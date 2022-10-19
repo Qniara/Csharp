8ef5dafe9ef9967d8b72af724901ddf4d6f1e926
17mm <114557943+PsychofankakrolaJuliana@users.noreply.github.com>
2022-10-19 09:31:07 +0200

    Add files via upload

---
+++ KartaPracy3a.cs
@@ -0,0 +1,49 @@
+﻿{
+    //Zad.1
+    //int n = int.Parse(Console.ReadLine());
+    //for (int i = 0; i<n; i++)
+    //{
+    //Console.WriteLine("*-|");
+    //}
+    //Zad.2
+    //for (int i=0; i<10; i++)
+    //{
+    //for (int j = 1; j <11; j++)
+    //{
+    //Console.WriteLine(i * j + "\t");
+    //}
+    //}
+    //Zad.3
+    //int n = int.Parse(Console.ReadLine());
+    //for (int i = 0; i < n; i++)
+    //{
+    //for (int j = 0; j < i; j++)
+    //{
+    //Console.Write("*");
+    //}
+    //Console.WriteLine();
+    //}
+    //Zad.
+    //int n = int.Parse(Console.ReadLine());
+    //for (int i = 0; i<n; i++)
+    //{
+        //for (int j = 0; j < n-i; j++)
+        {
+            //Console.Write("*");
+        }
+    }
+    int n = int.Parse(Console.ReadLine());
+    for (int i = 0; i<n; i++)
+    {
+        for (int j =0; j <n-j; j++)
+        {
+            Console.Write(" ");
+        }
+        for (int k = n-i-1; k<n; k++)
+        {
+            Console.Write("*");
+        }
+    }
+    Console.WriteLine();
+}
+    
