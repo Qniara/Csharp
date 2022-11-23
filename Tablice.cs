00da5fac71c398b5f1dd3a82818118a56decfbd3
16mm <114557943+Qniara@users.noreply.github.com>
2022-11-23 09:16:05 +0100

    Add files via upload
    awgva

---
+++ Tablice.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Niech zyje krol Julian!");
+// Tablice
+//int a = 3;
+//int b = 5;
+//int c = 6;
+//int d = 7;
+//int e;
+//int[] T = new int[4];
+//T[0] = 3;
+//T[1] = 5;
+//T[2] = 6;
+//T[3] = 7;
+//for (int i = 1; i < T.Length; i++)
+//{
+//    Console.WriteLine(T[i]);
+//}
+//int[] K = { 2, 3, 5, 8, 13, 21, 34, 55 };
+//for (int i = 0; i < K.Length; i++)
+//{
+//    Console.WriteLine($"{K[i]}");
+//}
+//int[] L;
+//L = new int[] { 1, 2, 3 };
+
+//Znajdz maksymalna wartosc tablicy [4,5,6,7,8,9]
+//int[] K = new int[] { 4, 5, 8, 9, 7, 6 };
+//int maks = 0;
+//for (int i = 0; i < K.Length; i++)
+//{
+//    if (K[i] > maks)
+//    {
+//        maks = K[i];
+//    }  
+//}
+//Console.WriteLine(maks);
+
+// Znajdz mini wartosc tablicy [4,5,6,7,8,9]
+//int[] K = new int[] { 4, 5, 8, 9, 7, 6 };
+//int mini = 999999;
+//for (int i = 0; i < K.Length; i++)
+//{
+//    if (K[i] < mini)
+//    {
+//        mini = K[i];
+//    }
+//}
+//Console.WriteLine(mini);
