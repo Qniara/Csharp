df7c3776a65260aaa820b91e6052031ce0512bda
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-01 08:06:30 +0100

    Create Karta_Pracy_6A.cs

---
+++ Kary Pracy cs/Karta_Pracy_6A.cs
@@ -0,0 +1,17 @@
+//Zad.1 - do sprawdzenia jeszcze
+//int suma = 0;
+//int n = int.Parse(Console.ReadLine());
+//int x = n;
+//for (int i = 0; x <= 1; i++)
+//{
+//    x = n % 10;
+//    suma = suma + x;
+//}
+////int i = 0;
+////while (x == 0)
+////{
+////    i++;
+////    x = n % 10;
+////    suma = suma + x;
+////}
+//Console.WriteLine(suma);
