1f82781f08bd939fd06664c1bd9d3185f94b3eaf
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-10 19:35:59 +0100

    Create Karta_Pracy_6.cs

---
+++ Kary Pracy cs/Karta_Pracy_6.cs
@@ -0,0 +1,16 @@
+//Zad.1
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//int x = c - b;
+//int y = b - a;
+//int k = a - b;
+//int l = b - c;
+//if(x == y || k==l)
+//{
+//    Console.Write("Podany ciag jest arytmetyczny.");
+//}
+//else
+//{
+//    Console.Write("Podany ciag nie jest arytmetyczny");
+//}
