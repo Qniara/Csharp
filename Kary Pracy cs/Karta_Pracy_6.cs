c8078be9248fd9830e50aa6fee3c1c4ebeeaea0f
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-10 19:44:20 +0100

    Update Karta_Pracy_6.cs

--- Kary Pracy cs/Karta_Pracy_6.cs
+++ Kary Pracy cs/Karta_Pracy_6.cs
@@ -34,3 +34,13 @@
 //{
 //    Console.WriteLine("Podany ciag jest rosnacy");
 //}
+//Zad.2
+//int suma = 0;
+//for(int i = 100; i < 1000; i++)
+//{
+//    if(i%8==0 && i % 16 != 0)
+//    {
+//        suma = suma + i;
+//    }
+//}
+//Console.WriteLine(suma);
