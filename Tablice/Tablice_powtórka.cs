3220e1fff5e3d863ef01aef4caa1a4b728dfd175
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-12 09:42:14 +0100

    Create Tablice_powtórka.cs

---
+++ Tablice/Tablice_powtórka.cs
@@ -0,0 +1,22 @@
+//1.Powtórka
+// Definiowanie Tablicy:
+//int[] T = new int[4]; //Tablica która ma 4 sloty (liczone od 0)
+//T[0] = 1; //Ustawanie elemntu 0 na wartość 1
+//T[1] = 2;
+//T[2] = 3;
+//int x = int.Parse(Console.ReadLine());
+//T[3] = x; // Element 3 na wartość x
+
+////Wypisanie kolejno elementów tablicy
+//for (int i = 0; i < T.Length; i++)
+//{
+//    Console.WriteLine(T[i]);
+//}
+//// Robienie randomowej tablicy
+//int[] K= new int[4];
+//Random r = new Random();
+//for(int i = 0; i < 4; i++)
+//{
+//    K[i] = r.Next();
+//    Console.WriteLine(K[i]);
+//}
