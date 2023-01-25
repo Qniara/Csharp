54b94494e197c58f3d79724eb06753c2b41d9f8f
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-25 09:31:54 +0100

    Update Funkcje.cs

--- Funkcje.cs
+++ Funkcje.cs
@@ -3,15 +3,21 @@
 //{
 //    instrukcje
 //}
-void przywitaj()
-{
-    Console.WriteLine("No elo elo");
-}
-przywitaj();
+//void przywitaj()
+//{
+//    Console.WriteLine("No elo elo");
+//}
+//przywitaj();
 
 //Napisz funkcje ktora oblicza sume przekazanych przez parametr 3 liczb
-void suma(int a, int b)
-{
-    Console.WriteLine(a + b);
-}
-suma(3, 7);
+//void suma(int a, int b)
+//{
+//    Console.WriteLine(a + b);
+//}
+//suma(3, 7);
+
+//int suma2(int x, int y)
+//{
+//    return x + y;
+//}
+//Console.WriteLine(suma2(2, 3));
