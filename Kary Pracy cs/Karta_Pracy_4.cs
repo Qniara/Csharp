5d989ddca87b3836353bb62a272892d4f44a81da
15mm <114557943+Qniara@users.noreply.github.com>
2023-01-26 20:09:17 +0100

    Create Karta_Pracy_4.cs

---
+++ Kary Pracy cs/Karta_Pracy_4.cs
@@ -0,0 +1,20 @@
+//Zad.1
+//NWD
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int NWD()
+//{
+//    while (a != b)
+//    {
+//        if (a > b)
+//        {
+//            a = a - b;
+//        }
+//        else if(b>a)
+//        {
+//            b = b - a;
+//        }
+//    }
+//    return a;
+//}
+//Console.WriteLine(NWD);
