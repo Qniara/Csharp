582b20f3ba6fae42d2cf34ed223d19c9f76872e7
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-15 09:11:52 +0100

    Update Tablice2.cs

--- Tablice/Tablice2.cs
+++ Tablice/Tablice2.cs
@@ -20,3 +20,19 @@
 //    Console.WriteLine(item + " ");
 //}
 //Console.WriteLine("\n");
+
+//Tablice dwuwymiarowe
+int[,] M = new int[2, 3];
+M[0, 0] = 3;
+M[0, 1] = 4;
+M[0, 2] = 5;
+M[1, 0] = 8;
+M[1, 1] = 9;
+M[1, 2] = 10;
+
+int[,] N = new int[4,3] { { 2, 3, 4 }, { 7, 8, 9 }, { 13, 14, 15 }, { 21, 31, 41 } };
+for(int i = 0; i < N.Length; i++) //0-11
+{
+    Console.Write(N[i / 3, i % 3] + " ");
+}
+Console.WriteLine("\n");
