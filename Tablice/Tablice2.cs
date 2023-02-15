900cbf1569f2c85c89affd63c4b480aff2d59cfb
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-15 09:00:10 +0100

    Update Tablice2.cs

--- Tablice/Tablice2.cs
+++ Tablice/Tablice2.cs
@@ -1,15 +1,22 @@
 //Tablice 
-int[] T = new int[3]; // deklaracja tablicy
-T[0] = 4;
-T[1] = 8;
-T[2] = 9;//deklaracja definiuje nazwe a definicja przypisuje wartość
+//int[] T = new int[3]; // deklaracja tablicy
+//T[0] = 4;
+//T[1] = 8;
+//T[2] = 9;//deklaracja definiuje nazwe a definicja przypisuje wartość
 
 //Deklaracja i definicja w jednym
-int[] D = new int[] { 5, 8, 12, 16 };
+
+//int[] D = new int[] { 5, 8, 12, 16 };
 
 //Przechodzenie po tablicy - 2 pętle (for i foreach)
-for(int i=0;i<D.Length; i++)
-{
-    Console.WriteLine(D[i] + " ");
-}
-Console.WriteLine("\n");
+//for (int i=0;i<D.Length; i++)
+//{
+//    Console.WriteLine(D[i] + " ");
+//}
+//Console.WriteLine("\n");
+
+//foreach (var item in D)
+//{
+//    Console.WriteLine(item + " ");
+//}
+//Console.WriteLine("\n");
