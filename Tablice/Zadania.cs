2203b32fbe66c57a9188584d063966435c666f32
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-22 08:42:43 +0100

    Update Zadania.cs

--- Tablice/Zadania.cs
+++ Tablice/Zadania.cs
@@ -18,13 +18,16 @@
 
 //Zad.2
 //Wygeneruj tablice 20 dowolnych liczb 2cyfrowych i zaokrąglij każdą w góre do pełnej 10
-int[] T = new int[20];
-Random r = new Random();
-for (int i = 0; i < 20; i++)
-{
-    T[i] = r.Next(10, 100);
-    if (T[i] % 2 == 1)
-    {
-        Console.WriteLine(i + ". " + T[i] + " ");
-    }
-}
+//int[] T = new int[20];
+//int x = 0;
+//Random r = new Random();
+//for (int i = 1; i < 21; i++)
+//{
+//    T[i] = r.Next(10, 100);
+//    x = 10 - T[i];
+//    T[i] = T[i] + x;
+//    Console.WriteLine(i + ". " + T[i] + " ");
+//}
+
+//Zad.3
+//Wygeneruj liste 12 losowych liczb dwucyfrowych podzielnych przez 7
