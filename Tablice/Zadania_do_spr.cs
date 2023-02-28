1fbd9eb722eabe26e2f2aee9b99f7f5deb5b0527
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-28 19:39:05 +0100

    Update Zadania_do_spr.cs

--- Tablice/Zadania_do_spr.cs
+++ Tablice/Zadania_do_spr.cs
@@ -13,3 +13,39 @@
 //{
 //    Console.Write(item + " ");
 //}
+
+//Zad.2
+//bool f = false;
+//int x = 0;
+//bool F(int x)
+//{
+//    for(int i = 2; i < x; i++)
+//    {
+//        if (x % i == 0)
+//        {
+//            f=true;
+//        }
+//    }
+//    if (f == true)
+//    {
+//        return true;
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
+//Random r= new Random();
+//int[] T = new int[20];
+//for(int i = 0; i < T.Length; i++)
+//{
+//    x = r.Next();
+//    if(F(x)==false && x%2==1 && x % 3 != 0 && x % 5 != 0 && x % 7 != 0)
+//    {
+//        T[i] = x;
+//    }
+//}
+//foreach(int j in T)
+//{
+//    Console.Write(T[j] + " ");
+//}
