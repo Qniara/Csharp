0e625dc4934ada3fff9387a5391350c032ff7be3
15mm <114557943+Qniara@users.noreply.github.com>
2023-02-28 19:27:11 +0100

    Update Zadania_do_spr.cs

--- Tablice/Zadania_do_spr.cs
+++ Tablice/Zadania_do_spr.cs
@@ -1,1 +1,15 @@
 //Zad.1
+//using System.Collections;
+//ArrayList A=new ArrayList();
+//int n=int.Parse(Console.ReadLine());
+//for(int i = 1; i <= n; i++)
+//{
+//    if (n % i == 0)
+//    {
+//        A.Add(i);
+//    }
+//}
+//foreach(var item in A)
+//{
+//    Console.Write(item + " ");
+//}
