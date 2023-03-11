8b10ededd12febe095d6e269de30c2b6ae6eed44
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-11 10:03:52 +0100

    Update KP_1_funkcja.cs

--- Funkcje/KP_1_funkcja.cs
+++ Funkcje/KP_1_funkcja.cs
@@ -6,3 +6,12 @@
 //    return x * x + y * y;
 //}
 //Console.WriteLine(a + "^2+" + b + "^2" + "=" + suma(a, b));
+
+//Zad.2
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int suma(int x, int y)
+//{
+//    return (x + y) * (x + y);
+//}
+//Console.WriteLine(suma(a, b));
