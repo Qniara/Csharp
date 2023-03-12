cf006a5c17d2c8bc55c22168160f7e6b4c4e94a2
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-12 08:20:09 +0100

    Update KP_2_funkcja.cs

--- Funkcje/KP_2_funkcja.cs
+++ Funkcje/KP_2_funkcja.cs
@@ -13,3 +13,19 @@
 //    return 0;
 //}
 //Console.WriteLine(f(n));
+
+//Zad.2
+//int n = int.Parse(Console.ReadLine());
+//int f(int x)
+//{
+//    if (x % 17 == 0 && 99<x && x<1000)
+//    {
+//        Console.WriteLine("TAK");
+//    }
+//    else
+//    {
+//        Console.WriteLine("NIE");
+//    }
+//    return 0;
+//}
+//Console.WriteLine(f(n));
