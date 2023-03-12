20a6f34de63728670ef314e5a1b365cdca1b944b
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-12 08:18:01 +0100

    Update KP_2_funkcja.cs

--- Funkcje/KP_2_funkcja.cs
+++ Funkcje/KP_2_funkcja.cs
@@ -1,1 +1,15 @@
 //Zad.1
+//int n = int.Parse(Console.ReadLine());
+//int f (int x)
+//{
+//    if (x % 3 == 0)
+//    {
+//        Console.WriteLine("TAK");
+//    }
+//    else
+//    {
+//        Console.WriteLine("NIE");
+//    }
+//    return 0;
+//}
+//Console.WriteLine(f(n));
