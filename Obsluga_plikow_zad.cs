579eb02b7f59cc5049fe2f06bce729944b452d81
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-20 14:00:36 +0100

    Update Obsluga_plikow_zad.cs

--- Obsluga_plikow_zad.cs
+++ Obsluga_plikow_zad.cs
@@ -1,1 +1,14 @@
+bool czypie(int x)
+{
+    bool flaga = true;
+    for(int i = 2; i < x; i++)
+    {
 
+        if (x % i == 0)
+        {
+            flaga = false;
+        }
+    }
+    return flaga;
+}
+string[] liczby = System.File.IO.ReadAllLines(@"sciezka do pliku");
