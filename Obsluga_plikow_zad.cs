24611ff7031a60e2f432141186cdfd527b358b63
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-20 14:13:36 +0100

    Update Obsluga_plikow_zad.cs

--- Obsluga_plikow_zad.cs
+++ Obsluga_plikow_zad.cs
@@ -4,3 +4,15 @@
 //foreach (string line in liczby) { wynik.WriteLine(line); }
 //wynik.Flush();
 //wynik.Close();
+//bool czypie(int x)
+//{
+//    bool flaga = true;
+//    for (int i = 2; i < x; int++)
+//    {
+//        if (x % i == 0)
+//        {
+//            flaga = false;
+//        }
+//    }
+//    return flaga;
+//}
