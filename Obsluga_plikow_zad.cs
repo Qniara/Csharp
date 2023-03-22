0394166402a0323c8c9b4318d4ec72e89ba30172
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-22 07:37:18 +0100

    Update Obsluga_plikow_zad.cs

--- Obsluga_plikow_zad.cs
+++ Obsluga_plikow_zad.cs
@@ -1,18 +1,21 @@
-//string[] liczby = System.IO.File.ReadAllLines(@"C:\Users\uczen\liczby.txt");
-//StreamWriter wynik = new(@"C:\Users\uczen\wyniki4.txt");
-//wynik.WriteLine("Zadanie\n");
-//foreach (string line in liczby) { wynik.WriteLine(line); }
-//wynik.Flush();
-//wynik.Close();
-//bool czypie(int x)
-//{
-//    bool flaga = true;
-//    for (int i = 2; i < x; int++)
-//    {
-//        if (x % i == 0)
-//        {
-//            flaga = false;
-//        }
-//    }
-//    return flaga;
-//}
+string[] liczby = System.IO.File.ReadAllLines(@"C:\Users\uczen\liczby.txt");
+StreamWriter wynik = new(@"C:\Users\uczen\wyniki4.txt");
+wynik.WriteLine("Zadanie\n");
+foreach (string line in liczby) 
+{ 
+    wynik.WriteLine(line); 
+}
+wynik.Flush();
+wynik.Close();
+bool czypie(int x)
+{
+    bool flaga = true;
+    for (int i = 2; i < x; i++)
+    {
+        if (x % i == 0)
+        {
+            flaga = false;
+        }
+    }
+    return flaga;
+}
