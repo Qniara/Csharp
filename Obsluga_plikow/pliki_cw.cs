90025789ff53f6025b70a48ead2a5a022ff2b3d4
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-26 08:49:01 +0200

    Update pliki_cw.cs

--- Obsluga_plikow/pliki_cw.cs
+++ Obsluga_plikow/pliki_cw.cs
@@ -1,1 +1,9 @@
-
+using System.IO;
+//var a = File.ReadAllText(@"C:\Users\admin\Desktop\Popr\C#\pliki\dokument.txt");
+//var b = File.ReadAllLines(@"C:\Users\admin\Desktop\Popr\C#\pliki\dokument.txt");
+////Console.WriteLine(plik);
+//Console.WriteLine();
+//var x = string.Join(Environment.NewLine, b);
+////Console.WriteLine(x);
+File.WriteAllText();
+File.WriteAllLines();
