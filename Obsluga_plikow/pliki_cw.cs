f7db57810034cd34b82c20d726a1a011f579e61e
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-26 09:09:48 +0200

    Update pliki_cw.cs

--- Obsluga_plikow/pliki_cw.cs
+++ Obsluga_plikow/pliki_cw.cs
@@ -1,10 +1,14 @@
 using System.IO;
+
+//Czytanie pliku
 //var a = File.ReadAllText(@"C:\Users\admin\Desktop\Popr\C#\pliki\dokument.txt");
 //var b = File.ReadAllLines(@"C:\Users\admin\Desktop\Popr\C#\pliki\dokument.txt");
 ////Console.WriteLine(plik);
 //Console.WriteLine();
 //var x = string.Join(Environment.NewLine, b);
 ////Console.WriteLine(x);
+
+//Zastepowanie tektu w pliku
 Console.WriteLine("Insert name:");
 var name = Console.ReadLine();
 var x = File.ReadAllText(@"C:/Users/admin/Desktop/Popr/C#/pliki/write.txt");
