b3c195658528a81ed60bd31aaad6186dd345586b
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-26 09:21:10 +0200

    Update pliki_cw.cs

--- Obsluga_plikow/pliki_cw.cs
+++ Obsluga_plikow/pliki_cw.cs
@@ -3,14 +3,17 @@ using System.IO;
 //Czytanie pliku
 //var a = File.ReadAllText(@"C:\Users\admin\Desktop\Popr\C#\pliki\dokument.txt");
 //var b = File.ReadAllLines(@"C:\Users\admin\Desktop\Popr\C#\pliki\dokument.txt");
-////Console.WriteLine(plik);
+////Console.WriteLine(a);
 //Console.WriteLine();
 //var x = string.Join(Environment.NewLine, b);
 ////Console.WriteLine(x);
 
 //Zastepowanie tekstu w pliku
-Console.WriteLine("Insert name:");
-var name = Console.ReadLine();
-var x = File.ReadAllText(@"C:/Users/admin/Desktop/Popr/C#/pliki/write.txt");
-var document = x.Replace("{name}", name) .Replace("{datetime}", DateTime.Now.ToString());
-File.WriteAllText($"C:/Users/admin/Desktop/Popr/C#/pliki/dokument-{name}.txt", document);
+//Console.WriteLine("Insert name:");
+//var name = Console.ReadLine();
+//var x = File.ReadAllText(@"C:/Users/admin/Desktop/Popr/C#/pliki/dokument.txt");
+//var document = x.Replace("{name}", name) .Replace("{datetime}", DateTime.Now.ToString());
+//File.WriteAllText($"C:/Users/admin/Desktop/Popr/C#/pliki/dokument-{name}.txt", document);
+
+//var y = File.ReadAllText($"C:/Users/admin/Desktop/Popr/C#/pliki/dokument-{name}.txt");
+//Console.WriteLine(y);
