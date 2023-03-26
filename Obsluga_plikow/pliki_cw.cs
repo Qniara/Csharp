37f8a7ccc9e6bbb48923e3069459e839b3e48864
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-26 09:12:58 +0200

    Update pliki_cw.cs

--- Obsluga_plikow/pliki_cw.cs
+++ Obsluga_plikow/pliki_cw.cs
@@ -8,9 +8,9 @@ using System.IO;
 //var x = string.Join(Environment.NewLine, b);
 ////Console.WriteLine(x);
 
-//Zastepowanie tektu w pliku
+//Zastepowanie tekstu w pliku
 Console.WriteLine("Insert name:");
 var name = Console.ReadLine();
 var x = File.ReadAllText(@"C:/Users/admin/Desktop/Popr/C#/pliki/write.txt");
-var document = x.Replace("{name}", name);
+var document = x.Replace("{name}", name) .Replace("{datetime}", DateTime.Now.ToString());
 File.WriteAllText($"C:/Users/admin/Desktop/Popr/C#/pliki/dokument-{name}.txt", document);
