d59fb4d93aa5f32bf7b9b9117abdce8743087373
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-29 09:08:20 +0200

    Update Zad_mat2020.cs

--- Zad_mat2020.cs
+++ Zad_mat2020.cs
@@ -1,1 +1,8 @@
-
+//using System.IO;
+//string[] S = File.ReadAllLines(@"C:\\Users\\uczen\\przyklad.txt");
+//foreach (string item in S)
+//{
+//    Console.WriteLine(item);
+//    string[] T = item.Split(" ");
+//    Console.WriteLine(T[1]);
+//}
