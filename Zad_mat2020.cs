16faa960913ce591309571612516a4fcef00a356
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-29 09:18:44 +0200

    Update Zad_mat2020.cs

--- Zad_mat2020.cs
+++ Zad_mat2020.cs
@@ -1,8 +1,23 @@
 //using System.IO;
 //string[] S = File.ReadAllLines(@"C:\\Users\\uczen\\przyklad.txt");
+//int[] L = new int[S.Length];
+//string[] N = new string[S.Length];
+//int i = 0;
 //foreach (string item in S)
 //{
-//    Console.WriteLine(item);
-//    string[] T = item.Split(" ");
-//    Console.WriteLine(T[1]);
+//    L[i] = int.Parse((item.Split(" "))[0]);
+//    N[i] = (item.Split(" "))[1];
+//    i++;
 //}
+
+//string[] LINIA = new string[2];
+//foreach (string item in S)
+//{
+//    LINIA=item.Split(" ");
+//    L[i] = int.Parse(LINIA[0]);
+//    N[i] = LINIA[1];
+//    i++;
+//}
+
+//// L -> to ma być tablica intów
+//// N -> to ma być tablica stringów
