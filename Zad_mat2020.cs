8509f813ab59afeecebb9441b37aaeb4cd852dd1
15mm <114557943+Qniara@users.noreply.github.com>
2023-03-29 09:34:01 +0200

    Update Zad_mat2020.cs

--- Zad_mat2020.cs
+++ Zad_mat2020.cs
@@ -1,20 +1,49 @@
+////Zad.1
 //using System.IO;
 //string[] S = File.ReadAllLines(@"C:\\Users\\uczen\\przyklad.txt");
 //int[] L = new int[S.Length];
 //string[] N = new string[S.Length];
-//int i = 0;
+//int g = 0;
 //foreach (string item in S)
 //{
-//    L[i] = int.Parse((item.Split(" "))[0]);
-//    N[i] = (item.Split(" "))[1];
-//    i++;
+//    L[g] = int.Parse((item.Split(" "))[0]);
+//    N[g] = (item.Split(" "))[1];
+//    g++;
 //}
 
-//string[] LINIA = new string[2];
-//foreach (string item in S)
+////string[] LINIA = new string[2];
+////foreach (string item in S)
+////{
+////    LINIA = item.Split(" ");
+////    L[i] = int.Parse(LINIA[0]);
+////    N[i] = LINIA[1];
+////    i++;
+////}
+
+////Zad.2
+//bool f(int x)
 //{
-//    LINIA = item.Split(" ");
-//    L[i] = int.Parse(LINIA[0]);
-//    N[i] = LINIA[1];
-//    i++;
+//    bool flaga = true;
+//    for (int k = 2; k<x; k++)
+//    {
+//        if (x % k == 0)
+//        {
+//            flaga = false;
+//        }
+//    }
+//    return flaga;
+//}
+//for  (int i = 0; i< L.Length; i++)
+//{
+//    if (L[i] > 4 && L[i] % 2 == 0)
+//    {
+//        for(int j=2; j < L[i]; j++)
+//        {
+//            if(f(j) && f(L[i] - j))
+//            {
+//                Console.WriteLine($"{L[i]} {j} {L[i] - 1}");
+//                break;
+//            }
+//        }
+//    }
 //}
