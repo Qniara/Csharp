47425def6eda680b380ede406836d9d7eea17d5e
15mm <114557943+Qniara@users.noreply.github.com>
2023-04-05 07:26:07 +0200

    Create do_spr.cs

---
+++ do_spr.cs
@@ -0,0 +1,20 @@
+////Funkcje
+////1
+//int n = int.Parse(Console.ReadLine());
+//int f(int x)
+//{
+//    int a = 0;
+//    int suma = 0;
+//    while (x < 2)
+//    {
+//        //a = x % 10;
+//        //x = x - a;
+//        //suma = suma + a;
+//        suma = suma + n % 10;
+//        n=n / 10;
+//    }
+//    return suma;
+//}
+//Console.WriteLine(f(n));
+
+////2
