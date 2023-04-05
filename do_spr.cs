fbb369d364ff31adb31141126e9a337deeb2cedb
15mm <114557943+Qniara@users.noreply.github.com>
2023-04-05 07:43:08 +0200

    Update do_spr.cs

--- do_spr.cs
+++ do_spr.cs
@@ -1,20 +1,38 @@
-////Funkcje
-////1
+//Funkcje
+//1
 //int n = int.Parse(Console.ReadLine());
 //int f(int x)
 //{
-//    int a = 0;
+//    //int a = 0;
 //    int suma = 0;
-//    while (x < 2)
+//    while (x > 0)
 //    {
 //        //a = x % 10;
 //        //x = x - a;
 //        //suma = suma + a;
 //        suma = suma + n % 10;
-//        n=n / 10;
+//        n = n / 10;
 //    }
 //    return suma;
 //}
 //Console.WriteLine(f(n));
 
-////2
+//2
+//string napis = "Basia";
+//char[] C = napis.ToCharArray();
+//Array.Reverse(C);
+//string wynik = new string(C);
+//Console.WriteLine(wynik);
+
+//3
+//int n = int.Parse(Console.ReadLine());
+//int f(int x)
+//{
+//    while (x > 0)
+//    {
+//        Console.Write(x + "-");
+//        x = x / 2;
+//    }
+//    return 0;
+//}
+//Console.WriteLine(f(n));
