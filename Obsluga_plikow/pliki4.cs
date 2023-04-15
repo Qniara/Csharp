5c35d87fde20e851566ebf9d1af39ffe261ea6a1
15mm <114557943+Qniara@users.noreply.github.com>
2023-04-15 20:24:21 +0200

    Update pliki4.cs

--- Obsluga_plikow/pliki4.cs
+++ Obsluga_plikow/pliki4.cs
@@ -1,1 +1,27 @@
+using System.IO;
 
+//var a = File.ReadAllText("C:/Users/admin/Desktop/Popr/C#/pliki-Basia.txt");
+//Console.WriteLine(a);
+
+//var b = Directory.GetFiles("C:/Users/admin/Desktop/Popr/C#/txt/", "pliki-Basia.txt", SearchOption.TopDirectoryOnly);
+//foreach (string file in b)
+//{
+//    File.AppendAllText(file, "Niech żyje król Julian");
+//}
+
+//string c = "C:/Users/admin/Desktop/Popr/C#";
+//using (StreamWriter sw = new StreamWriter(c))
+//{
+//    sw.WriteLine("JAJA");
+//}
+
+//var d = @"C:\\Users\\admin\\Desktop\\Popr\\C#\\basia.txt";
+//if (!File.Exists(d))
+//{
+//    File.Create(d);
+//}
+//var d2 = Directory.GetFiles(@"C:\\Users\\admin\\Desktop\\Popr\\C#\\", "basia.txt", SearchOption.TopDirectoryOnly);
+//foreach (var file in d2)
+//{
+//    File.AppendAllText(file, "JAJA");
+//}
