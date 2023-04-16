8730aefb0886e957aa0135e7146419fc310eb5ed
15mm <114557943+Qniara@users.noreply.github.com>
2023-04-16 12:14:31 +0200

    Create do_spr.cs

---
+++ Funkcje/do_spr.cs
@@ -0,0 +1,32 @@
+//1
+//Console.WriteLine("Insert number: ");
+//int n=int.Parse(Console.ReadLine());
+//int f(int x)
+//{
+//    int suma = 0;
+//    int y = 0;
+//    while (x < 1)
+//    {
+//        y = x % 10;
+//        suma = suma + y;
+//        x = x - y;
+//    }
+//    return suma;
+//}
+//Console.WriteLine(f(n));
+
+//2
+//Console.WriteLine("Insert number: ");
+//int x = int.Parse(Console.ReadLine());
+//int f(int n)
+//{
+//    while (n > 0)
+//    {
+//        Console.Write(n + " - ");
+//        n = n / 2;
+//    }
+//    return 0;
+//}
+//Console.Write(f(x));
+
+//3
