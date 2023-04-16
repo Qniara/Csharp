fd1dbb30241739d5d07bddf3faaf6333e285adb9
15mm <114557943+Qniara@users.noreply.github.com>
2023-04-16 12:19:15 +0200

    Update do_spr.cs

--- Funkcje/do_spr.cs
+++ Funkcje/do_spr.cs
@@ -30,3 +30,11 @@
 //Console.Write(f(x));
 
 //3
+//Console.WriteLine("Insert number: ");
+//int x = int.Parse(Console.ReadLine());
+//int reku(int n)
+//{
+//    if (n == 3) return 1;
+//    else return reku(n - 1) + 2;
+//}
+//Console.WriteLine(reku(x));
