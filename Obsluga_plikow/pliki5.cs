7b96217bff27811875cd2d9dbe483de6a4a735c1
15mm <114557943+Qniara@users.noreply.github.com>
2023-04-18 18:00:36 +0200

    Create pliki5.cs

---
+++ Obsluga_plikow/pliki5.cs
@@ -0,0 +1,13 @@
+//using System.IO;
+//var a = @"C:\Users\emend\Documents\Visual Studio 2022\kotek.txt";
+//if (!File.Exists(a))
+//{
+//    File.Create(a);
+//}
+//var b = Directory.GetFiles(@"C:\Users\emend\Documents\Visual Studio 2022\", "kotek.txt", SearchOption.TopDirectoryOnly);
+//foreach (var file in b)
+//{
+//    File.AppendAllText(file, DateTime.Now.ToString());
+//}
+//var c = File.ReadAllText(a);
+//Console.WriteLine(c);
