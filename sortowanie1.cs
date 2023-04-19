4e05d7fb2c2497a78e1e27e91474fda4d4ba9f71
15mm <114557943+Qniara@users.noreply.github.com>
2023-04-19 07:59:48 +0200

    Create sortowanie1.cs

---
+++ sortowanie1.cs
@@ -0,0 +1,21 @@
+//Sortowanie przez porownywanie
+
+//1. Bąbelkowe
+
+//2. Przez wybór
+
+//3. Przez wstawianie
+
+// Sortowania w czasie liniowym
+
+//4. Przez zliczanie
+
+//5. Kubełkowe
+
+// Przez dzielenie
+
+//6. Przez scalanie
+
+//7. Quicksort Hoare
+
+//8. Quicksort Lomuto
