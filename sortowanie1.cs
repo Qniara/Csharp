ae6dce1b1f81284833466222a139f2fd1b5368d1
15mm <114557943+Qniara@users.noreply.github.com>
2023-04-19 08:43:11 +0200

    Update sortowanie1.cs

--- sortowanie1.cs
+++ sortowanie1.cs
@@ -1,5 +1,6 @@
 //Stworzenie tablicy 10 losowych liczb od 1 do 20
 Random r = new Random();
+int n = 10;
 int[] T = new int[20];
 for (int i = 0; i < 20; i++)
 {
@@ -10,6 +11,20 @@ for (int i = 0; i < 20; i++)
 //Sortowanie przez porownywanie
 
 //1. Bąbelkowe
+//od prawej rozsnąco
+//int temp = 0;
+//for (int i = n - 1; i >= 0; i--)
+//{
+//    for(int j = 0; j < i-1; j++)
+//    {
+//        if (T[j] > T[j + 1])
+//        {
+//            temp = T[j];
+//            T[j] = T[j + 1];
+//            T[j + 1] = temp;
+//        }
+//    }
+//}
 
 //2. Przez wybór
 
@@ -30,3 +45,9 @@ for (int i = 0; i < 20; i++)
 //8. Quicksort Lomuto
 
 //Wyświetlanie posortowanej tablicy
+
+//Console.WriteLine("\n");
+//for(int i = 0; i < 20; i++)
+//{
+
+//}
