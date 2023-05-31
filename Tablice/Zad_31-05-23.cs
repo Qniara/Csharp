0eb4b2918c4704fbd9cb64c60d27f323901dc9b1
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-05-31 21:29:45 +0200

    Update Zad_31-05-23.cs

--- Tablice/Zad_31-05-23.cs
+++ Tablice/Zad_31-05-23.cs
@@ -1,1 +1,38 @@
 //Zad.1
+//Random r = new Random();
+//int[] T = new int[50];
+//int ilo = 0;
+//int ilo2 = 0;
+//for(int i = 0; i < T.Length; i++)
+//{
+//    T[i] = r.Next(-100,100);
+//}
+//for(int i=0;i<T.Length; i++)
+//{
+//    if (T[i] % 2 == 0)
+//    {
+//        Console.Write(T[i] + " ");
+//    }
+//    if (T[i] > 0)
+//    {
+//        ilo = ilo + T[i];
+//    }
+//    else if (T[i] < 0)
+//    {
+//        ilo2 = ilo2 + T[i];
+//    }
+//}
+//Console.WriteLine();
+//for (int i = 0; i < T.Length; i++)
+//{
+//    if (T[i] % 2 != 0)
+//    {
+//        Console.Write(T[i] + " ");
+//    }
+//}
+//Console.WriteLine();
+//if (ilo2 > 0)
+//{
+//    ilo = ilo / ilo2;
+//    Console.WriteLine(ilo);
+//}
