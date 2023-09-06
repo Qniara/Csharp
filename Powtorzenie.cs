3fb5e69a3f3d2e7f4b55105d40795afa4fb80ccc
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-09-06 13:03:04 +0200

    Create Powtorzenie.cs

---
+++ Powtorzenie.cs
@@ -0,0 +1,37 @@
+// Przed Diagnozą
+
+//Alorytmy
+
+//Napisz i przetestuje algorytm Eklidesa odejmowanie, modulo i sort bombelkowe
+//bez funkcji
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//while (a > b)
+//{
+//    if (a > b)
+//    {
+//        a = a - b;
+//    }
+//    else
+//    {
+//        b = b - a;
+//    }
+//}
+//Console.WriteLine(a);
+//z funkcją
+//void nwd(int a, int b)
+//{
+//    while (a > b)
+//    {
+//        if (a > b)
+//        {
+//            a = a - b;
+//        }
+//        else
+//        {
+//            b = b - a;
+//        }
+//    }
+//    Console.WriteLine(a);
+//}
+//nwd(a, b);
