6f13ffe04d1f849cafd7447055cc90a7abe60749
Ziemniak <114557943+Qniara@users.noreply.github.com>
2023-09-06 13:39:00 +0200

    Update Powtorzenie.cs

--- Powtorzenie.cs
+++ Powtorzenie.cs
@@ -50,36 +50,36 @@
 //Console.WriteLine(a);
 
 //Bombelkowe
-void sort(int[] T)
-{
-    int pom;
-    int n = T.Length;
-    for(int i= n-1; i>=0; i--)
-    {
-        for(int j = 0; j < i; j++)
-        {
-            if (T[j] > T[j + 1])
-            {
-                pom = T[j+1];
-                T[j + 1] = T[j];
-                T[j] = pom;
-            }
-        }
-    }
-}
-int[] T = new int[20];
-Random r=new Random();
-for(int i = 0; i < T.Length; i++)
-{
-    T[i] = r.Next(1,100);
-}
-for(int i = 0; i < 20; i++)
-{
-    Console.Write(T[i] + " ");
-}
-Console.WriteLine();
-sort(T);
-for (int i = 0; i < 20; i++)
-{
-    Console.Write(T[i] + " ");
-}
+//void sort(int[] T)
+//{
+//    int pom;
+//    int n = T.Length;
+//    for(int i= n-1; i>=0; i--)
+//    {
+//        for(int j = 0; j < i; j++)
+//        {
+//            if (T[j] > T[j + 1])
+//            {
+//                pom = T[j+1];
+//                T[j + 1] = T[j];
+//                T[j] = pom;
+//            }
+//        }
+//    }
+//}
+//int[] T = new int[20];
+//Random r=new Random();
+//for(int i = 0; i < T.Length; i++)
+//{
+//    T[i] = r.Next(1,100);
+//}
+//for(int i = 0; i < 20; i++)
+//{
+//    Console.Write(T[i] + " ");
+//}
+//Console.WriteLine();
+//sort(T);
+//for (int i = 0; i < 20; i++)
+//{
+//    Console.Write(T[i] + " ");
+//}
