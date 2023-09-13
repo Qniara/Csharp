3ce71596e0e16b596dadb2489a09895f8b30fe54
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-09-13 14:27:06 +0200

    Update cw2.cs

--- Diagno_cw/cw2.cs
+++ Diagno_cw/cw2.cs
@@ -1,12 +1,146 @@
+using System.Data;
+
+int NWD(int x, int y)
+{
+    while (x != y)
+    {
+        if (x > y)
+        {
+            x = x - y;
+        }
+        else
+        {
+            y = y - x;
+        }
+    }
+    return x;
+}
+int NWW(int x, int y)
+{
+    int z = x * y;
+    while (x != y)
+    {
+        if (x > y)
+        {
+            x = x - y;
+            z = z / x;
+        }
+        else
+        {
+            y = y - x;
+            z = z / y;
+        }
+    }
+    return z;
+}
 //1.User podaje NWD i NWW dwóch liczb. Napisz program, który znajdzie te liczby.
-int nwd=int.Parse(Console.ReadLine());
-int nww=int.Parse(Console.ReadLine());
+//int x = int.Parse(Console.ReadLine());
+//int y = int.Parse(Console.ReadLine());
+//int iloczyn = x * y;
+//for(int i=1; i<=iloczyn; i++)
+//{
+//    if(iloczyn %i==0 && NWD(i, iloczyn) == x && NWW(i, iloczyn/i) == y)
+//    {
+//        Console.WriteLine(i + " " + iloczyn / i);
+//    }
+//}
 
 
 //2. Sprawdź czy suma dwóch ułamków podanych przez usera jest ułamkiem niewłaściwym.
+//string pier = Console.ReadLine();
+//string drug = Console.ReadLine();
+//string[] P = pier.Split("/");
+//string[] D = drug.Split("/");
+//int a = int.Parse(P[0]);
+//int b = int.Parse(P[1]);
+//int c = int.Parse(D[0]);
+//int d = int.Parse(D[1]);
+//int mianownik = NWW(b, d);
+//int licz1 = mianownik * a / b;
+//int licz2 = mianownik * c / d;
+//if(licz1 + licz2 > mianownik)
+//{
+//    Console.WriteLine("Jes");
+//}
+//else
+//{
+//    Console.WriteLine("Niet");
+//}
+
 //3. Sprawdź czy wieksza jest suma dwóch wpisanych przez usera ułamków czy suma ich odwrotności.
+//string pier = Console.ReadLine();
+//string drug = Console.ReadLine();
+//string[] P = pier.Split("/");
+//string[] D = drug.Split("/");
+//int a = int.Parse(P[0]);
+//int b = int.Parse(P[1]);
+//int c = int.Parse(D[0]);
+//int d = int.Parse(D[1]);
+//int e = b;
+//int f = a;
+//int g = d;
+//int h = c;
+//int mian1 = NWW(b, d);
+//int mian2 = NWW(f, h);
+//int licz1 = mian1 * a / b;
+//int licz2 = mian1 * c / d;
+//int licz3 = mian2 * e / f;
+//int licz4 = mian2 * g / h;
+//int licznik = licz1 + licz2;
+//int licznik2 =licz3 + licz4;
+//int dupa = licznik / mian1;
+//int pupa = licznik2 / mian2;
+//if (dupa > pupa)
+//{
+//    Console.WriteLine("Normalna suma wieksza");
+//}
+//else
+//{
+//    Console.WriteLine("Odwrotna suma wieksza");
+//}
+
 //4. Sprawdź czy w danym napisie więcej jest spółgłosek czy samogłosek.
+//char[] samogloski ={'a', 'i', 'e', 'u', 'o', 'y'};
+//string napis = Console.ReadLine();
+//int ilosc = 0;
+//for(int i = 0; i < napis.Length; i++)
+//{
+//    if (samogloski.Contains(napis[i]))
+//    {
+//        ilosc++;
+//    }
+//    else
+//    {
+//        --ilosc;
+//    }
+//}
+//if(ilosc>0)
+//{
+//    Console.WriteLine("Wiecej samogflosek");
+//}
+//else
+//{
+//    Console.WriteLine("Wiecej spolgklosek");
+//}
+
 //5. Podaj której literki jest najwięcej w podanym napisie i jaka to jest literka.
+//string n = Console.ReadLine();
+//int[] T = new int[300];
+//int maks = 0;
+//foreach (char a in n)
+//{
+//    int kod = (int)a;
+//    T[kod] = T[kod] + 1;
+//}
+//for (int i = 0; i < T.Length; i++)
+//{
+//    if (T[i] > maks)
+//    {
+//        maks = i;
+//    }
+//}
+//char litera = (char)maks;
+//Console.WriteLine(litera);
 
 //6. Znajdź trzy takie liczby, których suma, a także suma każdej pary tych liczb jest kwadratem innej liczby.
 
@@ -33,5 +167,6 @@ int nww=int.Parse(Console.ReadLine());
 //znaleźć sobie miejsce na ławce.
 //Niech k oznacza największą liczbę osób siedzących na jednej ławce (po przyjściu tych dodatkowych m osób). 
 //Jaka jest minimalna, a jaka maksymalna wartość k?
+
 
 Console.WriteLine("DUPA");
