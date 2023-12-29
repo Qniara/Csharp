1caa8e4e97f24b5693eacc3cf29f10eee0826e0d
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-12-29 13:13:21 +0100

    Update cw_z_lekcji.cs

--- Funkcje/Rekurencja/cw_z_lekcji.cs
+++ Funkcje/Rekurencja/cw_z_lekcji.cs
@@ -1,8 +1,24 @@
 //Zad.1
 //Zapisz iteracyjnie i rekurencyjnie obliczanie sumy kodów ASCII wpisanego przez usera słowa
+//string n = Console.ReadLine();
+//int suma = 0;
+//foreach(var i in n)
+//{
+//    suma += (char)i;
+//}
+//Console.Write(suma);
+
+//int zad1(string n)
+//{
+//    if (n == "") return 0;
+//    return (int)n[0] + zad1(n.Substring(1));
+//}
+//Console.WriteLine(zad1("A"));
 
 //Zad.2
 //Napisz iteracyjnie i rekurencyjnie obliczenie x^n (x do potęgi n)
 
+
 //Zad.3
 //Napisz algorytmy NWD modulo i odejmowanie w wersji reku
+
