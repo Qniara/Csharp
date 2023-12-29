e2375754d61c3f61423708a2cbd241bfaec78b2c
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2023-12-29 12:52:44 +0100

    Create cw_z_lekcji.cs

---
+++ Funkcje/Rekurencja/cw_z_lekcji.cs
@@ -0,0 +1,8 @@
+//Zad.1
+//Zapisz iteracyjnie i rekurencyjnie obliczanie sumy kodów ASCII wpisanego przez usera słowa
+
+//Zad.2
+//Napisz iteracyjnie i rekurencyjnie obliczenie x^n (x do potęgi n)
+
+//Zad.3
+//Napisz algorytmy NWD modulo i odejmowanie w wersji reku
