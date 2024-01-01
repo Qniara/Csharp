c9f285f4c35f6e7df9ee9ff79911121136eafe5d
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-01 18:24:53 +0100

    Update cw_z_lekcji.cs

--- Funkcje/Rekurencja/cw_z_lekcji.cs
+++ Funkcje/Rekurencja/cw_z_lekcji.cs
@@ -17,8 +17,58 @@
 
 //Zad.2
 //Napisz iteracyjnie i rekurencyjnie obliczenie x^n (x do potęgi n)
+//Console.Write("Podaj liczbe: ");
+//int x = int.Parse(Console.ReadLine());
+//int x2 = x;
+//Console.Write("Podaj liczbe: ");
+//int n = int.Parse(Console.ReadLine());
+//for(int i=1; i<n; i++)
+//{
+//    x = x * x2;
+//}
+//Console.WriteLine(x);
 
+//int zad2(int x, int n)
+//{
+//    if (n > 0)
+//    {
+//        return x * zad2(x, n - 1);
+//    }
+//    else
+//    {
+//        return 1;
+//    }
+//}
+//Console.WriteLine(zad2(2, 3));
 
 //Zad.3
 //Napisz algorytmy NWD modulo i odejmowanie w wersji reku
+//int zad3a(int a, int b)
+//{
+//    if (a > b)
+//    {
+//        return zad3a(a - b, a);
+//    }
+//    else if (a < b)
+//    {
+//        return zad3a(a, b - a);
+//    }
+//    else
+//    {
+//        return a;
+//    }
+//}
+//Console.WriteLine(zad3a(25, 10));
 
+//int zad3b(int a, int b)
+//{
+//    if (b > 0)
+//    {
+//        return zad3b(b, a % b);
+//    }
+//    else
+//    {
+//        return a;
+//    }
+//}
+//Console.WriteLine(zad3b(20, 10));
