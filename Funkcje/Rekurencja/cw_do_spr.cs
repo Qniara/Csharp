b97ffa0b52fe9c04856baaba313209c88fe05286
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-01-06 19:04:03 +0100

    Update cw_do_spr.cs

--- Funkcje/Rekurencja/cw_do_spr.cs
+++ Funkcje/Rekurencja/cw_do_spr.cs
@@ -1,1 +1,31 @@
 //Zad.1
+//Reku nww i nwd
+int nwd_odej(int a, int b)
+{
+    if (a < b)
+    {
+        return nwd_odej(a, b - a);
+    }
+    if (a > b)
+    {
+        return nwd_odej(a - b, b);
+    }
+    return a;
+}
+
+int nwd_mod(int a, int b)
+{
+    if (b > 0)
+    {
+        return nwd_mod(b, a%b);
+    }
+    return a;
+}
+
+int nww(int a, int b)
+{
+    return a * b / nwd_odej(a, b);
+}
+
+
+//Console.WriteLine(nww(2, 4));
