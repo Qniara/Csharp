578dbfbe9e50189f52f385da20418131aa327593
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-03-13 13:54:46 +0100

    Create cw_13_03_2024.cs

---
+++ Lekcje/cw_13_03_2024.cs
@@ -0,0 +1,10 @@
+/*KLASY i FASADY
+ * Kompozycja - scisle połączenie 
+ *  - np. Kombajn i gitara
+ *  - podlaczenie obiektu w konstruktorze
+ * Agregacja - porządane połączenie 
+ *  - np. Kombajn i zapasowa gitara
+ *  - podlaczenie w set
+ * Asocjacja - dowolnie, nie niezbędne połączenie
+ *  - np. Kombajn i wszystkie struny w gitarze
+ */
