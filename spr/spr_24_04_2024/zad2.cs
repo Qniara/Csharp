96fad272b0ddf6458c55028e1473824c38300ad0
Marta Mendel <114557943+Qniara@users.noreply.github.com>
2024-04-24 15:05:34 +0200

    Add files via upload

---
+++ spr/spr_24_04_2024/zad2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spr2
+{
+    public abstract class Autobus { }
+    public class Przystanek
+    {
+        bool czyNaRzadanie;
+        bool czyPosiadaBiletomat;
+        bool czyPosiadaZadaszenie;
+        List<Autobus> ListaAutobusow;
+    }
+    class PunktCentralny : Przystanek
+    {
+
+    }
+    class Petla : Przystanek
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
